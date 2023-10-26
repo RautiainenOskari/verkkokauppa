49b8ae4bf053e0fec15e216f7e748d5e9295f16e
SpookySkoops <rautiainen.oskarir@gmail.com>
2023-10-26 15:40:43 +0300

    First commit

---
+++ verkkokauppa/src/main/java/com/projekti/verkkokauppa/VerkkokauppaApplication.java
@@ -0,0 +1,13 @@
+package com.projekti.verkkokauppa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerkkokauppaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerkkokauppaApplication.class, args);
+	}
+
+}
---
+++ verkkokauppa/src/test/java/com/projekti/verkkokauppa/VerkkokauppaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projekti.verkkokauppa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerkkokauppaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
